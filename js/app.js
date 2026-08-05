70ccaf06a33f8ab250cd90b8e6f55b1b6316b06c
Lee Forde <lee.forde98@icloud.com>
2026-08-05 14:20:46 +0100

    Refactor portfolio architecture and complete homepage v1

--- js/app.js
+++ js/app.js
@@ -23,3 +23,29 @@ document.querySelectorAll("section").forEach(section => {
     observer.observe(section);
 
 });
+
+/* ==========================================
+   Scroll Animation
+========================================== */
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.15
+});
+
+document.querySelectorAll(".fade-up").forEach(element=>{
+
+    observer.observe(element);
+
+});
